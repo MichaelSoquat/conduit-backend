610f4a5a025ac88cdbb74eb71a8e11538b84cfea
MichaelSoquat <michael.soquat@gmx.de>
2024-10-29 10:41:59 +0100

    Updated req!

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
