fce5eb80651988804d436ab802cafbba36d98e9f
MichaelSoquat <michael.soquat@gmx.de>
2024-11-03 14:47:57 +0100

    updated entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+python manage.py createsuperuser --noinput \
+  --email "$DJANGO_SUPERUSER_EMAIL" \
+  --username "$DJANGO_SUPERUSER_USERNAME"
 
-gunicorn conduit-backend.wsgi:application --bind 0.0.0.0:8000
+gunicorn conduit.wsgi:application --bind 0.0.0.0:8000
