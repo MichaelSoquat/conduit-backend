05255d8d23bba84ad594bf64ccd75ba9920c3546
MichaelSoquat <michael.soquat@gmx.de>
2024-12-15 16:13:23 +0100

    added collect static

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ set -e
 python manage.py migrate
 python manage.py makemigrations
 python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME"
+python manage.py collectstatic --noinput
         
 exec python manage.py runserver 0.0.0.0:8000
