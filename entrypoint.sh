e7c3d46d13dee30cb8e6be8b6923b266db97f161
MichaelSoquat <michael.soquat@gmx.de>
2024-12-15 16:16:40 +0100

    Added gunicorn server

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py makemigrations
 python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL" --username "$DJANGO_SUPERUSER_USERNAME"
 python manage.py collectstatic --noinput
         
-exec python manage.py runserver 0.0.0.0:8000
+gunicorn conduit.wsgi:application --bind 0.0.0.0:8000
